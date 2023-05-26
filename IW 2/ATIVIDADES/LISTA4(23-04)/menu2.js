7c19e4a13627773f19b9e94fbefc38a60f5ee802
Gustavo <saikykusuonopsiman@gmail.com>
2023-05-25 23:19:46 -0300

    alterando

---
+++ IW 2/ATIVIDADES/LISTA4(23-04)/menu2.js
@@ -0,0 +1,57 @@
+function openPage() {
+    let headerMenu = document.getElementById('headerMenu');
+    let triangulo1 = document.getElementById('triangulo1');
+    let Menu1 = document.getElementById('Menu1');
+    let MenuThorfDivine = document.getElementById('MenuThorfDivine');
+    let triangulo2 = document.getElementById('triangulo2');
+    let nav = document.getElementById('nav-list');
+
+    headerMenu.style.transition = "0.5s";
+    headerMenu.style.padding = "0 0 0 27%";
+    Menu1.style.width = "20em"
+    
+
+    if(Menu1.style.width == "20em" ){
+        setTimeout(function (params) {
+            triangulo1.style.display = "block";
+            triangulo2.style.display = "block";
+        }, 90);
+       
+    }
+    
+
+}
+
+function ThorfDivineOpen(){
+    let headerMenu = document.getElementById('headerMenu');
+    let triangulo1 = document.getElementById('triangulo1');
+    let Menu1 = document.getElementById('Menu1');
+    let MenuThorfDivine = document.getElementById('MenuThorfDivine');
+    let triangulo2 = document.getElementById('triangulo2');
+    let nav = document.getElementById('nav-list');
+
+    headerMenu.style.transition = "0.5s";
+    headerMenu.style.padding = "0 0 0 25%";
+    Menu1.style.width = "24em";
+    nav.style.margin = "0 0 0 31.1%"
+    nav.style.width = "24em"
+    
+}
+
+
+function ThorfDivineClose(){
+    let headerMenu = document.getElementById('headerMenu');
+    let triangulo1 = document.getElementById('triangulo1');
+    let Menu1 = document.getElementById('Menu1');
+    let MenuThorfDivine = document.getElementById('MenuThorfDivine');
+    let triangulo2 = document.getElementById('triangulo2');
+    let nav = document.getElementById('nav-list');
+
+    headerMenu.style.transition = "0.5s";
+    headerMenu.style.padding = "0 0 0 27%";
+    Menu1.style.width = "20em";
+    nav.style.margin = "0 0 0 33.1%"
+    nav.style.width = "20em"
+
+}
+
