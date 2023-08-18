f32d978767286a119c5c42deb8d3354dc0c488f5
ThorfDivine <110639365+ThorfDivine@users.noreply.github.com>
2023-08-18 14:23:34 -0300

    banco

---
+++ SW 1/teste/conectando no banco de dados/busca_filtro.php
@@ -0,0 +1,12 @@
+<?php 
+    include('conectarPhp.php');
+    $valor = $_POST['valurBusca'];
+    $busca = mysqli_query($conexao, "select * from usuario where usarname = '$valor'");
+    $resultado;
+    while($resultado = mysqli_fetch_row($busca)){
+        echo $resultado[0] ;
+        echo "<br/>";
+        echo $resultado[1];
+        echo "<br/>";
+    }
+?>
