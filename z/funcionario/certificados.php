fea23d26d41f8a3c1382d757dd8e68adbcc137f4
Gustavo <saikykusuonopsiman@gmail.com>
2023-11-05 23:57:55 -0300

    asd

--- z/funcionario/certificados.php
+++ z/funcionario/certificados.php
@@ -17,11 +17,7 @@ if(
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BartoHelp - Meus Currículos</title>
-    <!-- Fav-Icon -->
-        <link rel="shortcut icon" href="../contents/favIcon/favicon.ico" type="image/x-icon">
-    <!-- end -->
-    
+    <title>BartoHelp - Certificados</title>
     <!-- Material Icons -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <!-- End -->
@@ -29,24 +25,24 @@ if(
     <!-- CSS -->
         <link rel="stylesheet" href="../../style/style.css">
         <link rel="stylesheet" href="../../style/header.css">
-        <link rel="stylesheet" href="../../style/meusCurriculos.css">
+        <style>
+            section{
+                height: 80vh;
+            }
+        </style>
     <!-- End -->
 </head>
 <body>
     <header></header>
     
     <section class="bigMarginTop bigMarginBotom centralize">
-        <div class="respostaElse">
-            <!-- Essa div só deve ser criada (junto ao seu conteúdo) caso não haja currículos 
-                 cadastrados previamente pelo usuário, estes cujo, registrados no banco de dados -->
-            <h1>Ao que parece você não possui currículos cadastrados</h1>
-            <h3>Clique <a href="../../HTML/funcionario/curriculos.php">aqui</a> para iniciar sua jornada!</h3>
-        </div>
-        
-        <div class="vagas">
-
-        </div>
-</section>
+        <form action="../certificados.php" methot="post">
+            <div class="flexC">
+                <label style="margin: 5px 25px;" for="certificado"> Seu Certificado: </label>
+                    <input style="display: block;" type="file" name="certificado" id="certificado" class="inputPattern">
+            </div>
+        </form>
+    </section>
 
     <footer></footer>
 </body>
---
+++ z_Novembro/php/endCurriculos.php
@@ -0,0 +1,163 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BartoHelp - Currículos</title>
+        <!-- Fav-Icon -->
+            <link rel="shortcut icon" href="../../contents/favIcon/favicon.ico" type="image/x-icon">
+        <!-- end -->
+        
+        <!-- Material Icons -->
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <!-- End -->
+    <!-- ==================================================================================== -->
+        <!-- CSS -->
+            <link rel="stylesheet" href="../../style/style.css">
+            <link rel="stylesheet" href="../../style/home.css">
+            <link rel="stylesheet" href="../../style/header.css">
+            <link rel="stylesheet" href="../../style/curriculos.css">
+            <link rel="stylesheet" href="../../style/endCurriculos.css">
+        <!-- End -->
+</head>
+<body>
+    <header id="top"></header>
+
+    <section class="flexC spaceBetween alingCenter bigMarginBotom">
+
+        <div style="width: 100%; height: 5vh; color: rgb(60,60,60);" class="centralize">
+            <h2>Quase acabando!</h2>
+        </div>
+
+        <div class="form flex marginBtm23px" style="margin-bottom: 45px;">
+            <form action="#" class="curriculo flexC" method="post">
+                <div class='flexR w100prc spaceAround'>
+                    <div class="flexC conteiner_item" style="width: 40%;">
+                        <div class="flexR marginBtm5px">
+                            <label for="experiencias" class="lable">Experiencias: </label>
+                        </div>
+                        <div class="flexR alingCenter experiencias" style="width: 100%;">
+                            <input type="text" name="experiencias" id="experiencias" class="inputPattern noMarginBottom">
+                                <span class="material-symbols-outlined add">add</span>
+                        </div>
+                    </div>
+                    <div class="flexC conteiner_item" style="width: 40%;">
+                        <div class="flexR marginBtm5px">
+                            <label for="idiomas" class="lable">Idiomas: </label>
+                        </div>
+                        <div class="flexR alingCenter experiencias" style="width: 100%;">
+                            <input type="text" name="idiomas" id="idiomas" class="inputPattern noMarginBottom">
+                                <span class="material-symbols-outlined add">add</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="competencias w100prc centralize marginTop23px marginBtm23px">
+                    <div class="flexC alingCenter spaceEvenly" style="width: 70%; background-color: rgba(60,60,60,0.2); border-radius: 4px;">
+                        <div class="tittle w100prc flexR alingCenter" id="cTitle">
+                            <h1>Nome da Competência</h1>
+                        </div>
+
+                        <div id="competenciaId" style="display: none;">
+
+                            <!-- Use o ajax//php pra deixar o id da competência nesse h2 -->
+
+                                <h2>16</h2>
+
+                        </div>
+
+                            <div class="explain" id="explain">
+                                <p>detalhes da competência: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum consectetur adipisicing elit.</p>
+                            </div>
+                        <div class="points flexC spaceBetween">
+                            <p id="nivel" class="centralize" style="margin-bottom: 15px;">Nível de competência: <i style="margin-left: 5px;">0</i></p>
+                            <div class="w100prc centralize">
+                                <div class="flexR spaceAround" style="width: 70%;">
+                                    <div class="redPoint" id="u"></div> <!--  u m     -->
+                                    <div class="redPoint" id="d"></div> <!--  d ois   -->
+                                    <div class="redPoint" id="t"></div> <!--  t rês   -->
+                                    <div class="redPoint" id="q"></div> <!--  q uatro -->
+                                    <div class="redPoint" id="c"></div> <!--  c inco  -->
+                                        <!-- redo -->
+                                </div>
+                                <div class="refresh">
+                                    <span class="material-symbols-outlined" id="refresh"> refresh </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="centralize marginTop23px">
+                    <input type="submit" value="Gerar Currículo" class="buttonPattern" style="width: 20%; height: 50px;">
+                </div>
+            </form>
+        </div>
+
+        <div></div>
+    </section>
+
+    <a href="#top">
+        <div id="backTop" class="backTop">
+            <div class="aqui" id="aqui" style="display: none;"></div>
+        </div>
+    </a>
+
+    <footer></footer>
+</body>
+<script>
+    // criando váriaveis conteiner -> Classificação de competência
+    let c1
+        c1 = 0
+    let c2
+        c2 = 0
+    let c3
+        c3 = 0
+    let c4
+        c4 = 0
+    let c5
+        c5 = 0
+    let c6
+        c6 = 0
+    let c7
+        c7 = 0
+    let c8
+        c8 = 0
+    let c9
+        c9 = 0
+    let c10
+        c10 = 0
+    let c11
+        c11 = 0
+    let c12
+        c12 = 0
+    let c13
+        c13 = 0
+    let c14
+        c14 = 0
+    let c15
+        c15 = 0
+    let c16
+        c16 = 0
+
+</script>
+    <script src="../../js/endCurriculo.js"></script>
+    <script src="../../js/header.js"></script>
+    <script src="../../js/footer.js"></script>
+    <script src="../../js/createBttn.js"></script>
+</html>
+
+
+<!-- 
+
+
+                        <div class="flexC conteiner_item">
+                            <div class="flexR marginBtm5px">
+                                <label for="idiomas" class="lable">Idiomas: </label>
+                            </div>
+                            <div class="flexR alingCenter experiencias">
+                                <input type="text" name="idiomas" id="idiomas" class="inputPattern noMarginBottom">
+                                    <span class="material-symbols-outlined add">add</span>
+                            </div>
+                        </div>
+
+ -->
